5a157fc3faaa523bd057dd44c12929d832af9274
Alexandre Lima <alexandrevl@gmail.com>
2022-04-06 15:36:03 -0300

    rules for banco do brasil

--- stream.js
+++ stream.js
@@ -9,7 +9,7 @@ const rulesURL = "https://api.twitter.com/2/tweets/search/stream/rules";
 const streamURL =
   "https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics";
 
-const rules = [{ value: '"bbb"' }];
+const rules = [{ value: '"banco do brasil"' }];
 //console.log(TOKEN);
 
 // Get stream rules
