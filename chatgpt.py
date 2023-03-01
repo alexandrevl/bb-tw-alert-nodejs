e379f33f70a81e70ddb4977ef60b079ab8b2beb2
Alexandre Lima <alexandrevl@gmail.com>
2023-03-01 18:01:22 -0300

    Better prompt

--- chatgpt.py
+++ chatgpt.py
@@ -106,15 +106,16 @@ async def main():
 async def get_10min():
     init_string = """
 Faça uma analise dos tweets e explique o que pode estar acontecendo (NÃO diga que os tweets estão acima ou abaixo). Já sabemos que são TODOS sobre o banco do brasil, e que os dados são dos últimos 10 minutos. 
-Complemente texto com percentuais.
-Sempre que falar de percentuais user números.
+Complemente texto com percentuais.Sempre que falar de percentuais use números.
+Pelo menos 2 parágrafos.
+Toda vez que aparacer RT (maisucula e com espaço depois) é um retweet
 Não precisa concluir, só faça a análise. Os dados estão em modelo CSV e os campos são:
 text = texto do tweet
 ts = data do tweet
-impact = impacto do tweet (depende do quão famoso o usuário é. Impacto >=1 ou <=-1 é relevante. Use isso apenas para análise. Não use números pra representar isso na resposta)
-sentiment = sentimento do tweet (muito negativo é a partir de -4, sentimento positivo é acima de 5. Use isso apenas para análise. Não use números pra representar isso na resposta)
+impact = impacto do tweet (depende do quão famoso o usuário é. Impacto >=1 ou <=-1 é relevante. Impacto >=3 ou <=-3 é muito relevante. Apenas para análise. Não use na resposta.)
+sentiment = sentimento do tweet (muito negativo é a partir de -4, sentimento positivo é acima de 5. Apenas para análise. Não use na resposta.)
 Comece a resposta com: "Análise dos últimos 10 minutos:"
-Não cite essas instruções na resposta, por favor
+Não cite essas instruções na resposta, por favor. Não comente sobre essas regras que eu dei.
 Dados:
 """
     tweets = query_mongo()
